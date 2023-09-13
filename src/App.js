82d7e98296dd2094fece5c9ea57af55b038ce165
Rishi Tiwari <rishitiwari@Rishis-MacBook-Pro.local>
2023-09-13 16:47:42 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
-import React, { useState, useEffect } from "react";
-
-function App() {
-  const [message, setMessage] = useState("");
-  const [websocket, setWebsocket] = useState(null);
-
+import React from 'react';
+import { useEffect } from 'react';
+export default function IndexPage() {
   useEffect(() => {
-    const socket = new WebSocket("ws://localhost:3000");
-
-    socket.onopen = () => {
-      console.log("WebSocket connection established.");
-    };
-
-    socket.onmessage = (event) => {
-      console.log("Received:", event.data);
-    };
-
-    setWebsocket(socket);
-
-    return () => {
-      socket.close();
-    };
-  }, []);
-
-  const sendMessage = () => {
-    if (websocket) {
-      websocket.send(message);
-      setMessage("");
-    }
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={sendMessage}>Send to Flutter</button>
-    </div>
-  );
+    console.log('heyyaaa');
+    if (window.Barcode && window.Barcode.postMessage) {
+      Barcode.postMessage("hello bro hi");
+   }
+  });
+  return <div>Hello World.</div>;
 }
-
-export default App;
